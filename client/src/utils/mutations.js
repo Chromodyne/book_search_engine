f1ef26f99254aa4ff7e51c89060c51e64862df5b
chromodyne <dsinitiere@outlook.com>
2022-08-25 18:38:10 -0700

    Added placeholder mutation names in front-end mutations.js.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,11 @@
+import { gql } from "graphql";
+
+//TODO: IMPORTANT, THESE ARE PLACEHOLDERS. Figure out logic.
+export const LOGIN_USER = ;
+
+export const ADD_USER = ;
+
+export const SAVE_BOOK = ;
+
+export const REMOVE_BOOK = ;
+
