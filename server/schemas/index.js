7a820582581e2c059663236f5f6a104e44beffc3
chromodyne <dsinitiere@outlook.com>
2022-08-25 17:39:27 -0700

    Most necessary files and directories created. Dependencies added.

---
+++ server/schemas/index.js
@@ -0,0 +1,7 @@
+//Import typedefs and resolvers.
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+
+//Export typeDefs and resolvers.
+module.exports = { typeDefs, resolvers };
+
