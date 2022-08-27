0d123ebe2f7cf5814855e8e9d0eed0d4e06d81bb
chromodyne <dsinitiere@outlook.com>
2022-08-27 11:09:29 -0700

    Created skeleton for resolvers.js.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,5 +3,31 @@ const { signToken } = require("../utils/auth.js");
 
 //TODO: Build out resolvers logic.
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            const findUser = await User.findOne({ _id: context.user.id }).select("-password");
+            
+            return findUser;
+        }
+    },
 
+    Mutation: {
+        login: async (parent, args, context) => {
+
+        },
+
+        addUser: async (parent, args, context) => {
+
+        },
+
+        removeBook: async (parent, args, context) => {
+
+        },
+
+        saveBook: async (parent, args, context) => {
+
+        }
+    }
 }
+
+module.exports = resolvers;
