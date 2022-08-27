dcc93107d8cb6f59392985e8d00487e7bdb2f6eb
chromodyne <dsinitiere@outlook.com>
2022-08-27 16:27:05 -0700

    Began work on query so I can test resolver implementation. Need to figure out how to deal with savedBook array.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,15 @@
 import { gql } from "graphql";
 
-export const QUERY_ME = ; 
+//User query.
+
+//TODO: Look into how to get the savedBooks array from the user.
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+        }
+    }
+
+`; 
