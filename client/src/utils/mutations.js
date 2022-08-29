aa11b267791a69f3d903a912d666aec2c097fa22
chromodyne <dsinitiere@outlook.com>
2022-08-28 19:39:58 -0700

    Realize I need to actually update the damn components to use mutations instead of the old API.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,16 @@ export const LOGIN_USER = gql`
     }
 `;
 
-// export const ADD_USER = ;
+export const ADD_USER = `gql 
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+
+`;
 
 // export const SAVE_BOOK = ;
 
