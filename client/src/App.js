657f029e9a96ab2588559aa920fdbe383d96f9cf
chromodyne <dsinitiere@outlook.com>
2022-08-28 18:22:19 -0700

    Began client side setup before moving back to resolvers.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,24 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//Import Apollo stuff for client.
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink
 } from "@apollo/client";
+
+//Create http link per docs.
+const link = createHttpLink({
+  uri: "/graphql"
+});
+
+//Apollo client setup.
+//TODO: Need to setup the auth link stuff per docs.
+// const client = new ApolloClient({
+
+// }
 
 function App() {
   return (
